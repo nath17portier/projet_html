d6d4f8cfabc410c80ebd6b44c6a126613726611c
Amelie Bret <amelie.bret@etud.univ-angers.fr>
2021-10-13 12:00:06 +0200

    Creation du component main menu

--- projet-html/src/app/app.module.ts
+++ projet-html/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
