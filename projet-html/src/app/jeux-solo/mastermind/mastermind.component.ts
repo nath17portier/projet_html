ed70cd4b98554abaeda9c0eaf0fbc6fd25552f05
Amelie Bret <amelie.bret@etud.univ-angers.fr>
2022-01-10 15:00:40 +0100

    Mise en place mastermind

--- projet-html/src/app/jeux-solo/mastermind/mastermind.component.ts
+++ projet-html/src/app/jeux-solo/mastermind/mastermind.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-mastermind',
@@ -7,28 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MastermindComponent implements OnInit {
 
-  listeCouleurs = ["yellow", "blue", "red", "orange", "pink", "purple"];
+  listeCouleurs = ["purple","blue", "green", "yellow", "red", "pink"];
   code =["","","",""];
-  couleursChoisies =["","","",""];
-  currentColorIdx =0;
+  listeTentatives: any = [];
+  listeResultats: any = [];
+  submittedCode: string[] = [];
+  submittedResult: string[] = [];
+  fb: FormBuilder = new FormBuilder;
   won=false;
+  lost=false;
+  formCouleurs: any;
 
   constructor() { }
 
   ngOnInit(): void {
     this.creerCode();
+    this.formCouleurs= this.fb.group({
+      couleur1: new FormControl("", Validators.required) ,
+      couleur2: new FormControl("", Validators.required) ,
+      couleur3: new FormControl("", Validators.required) ,
+      couleur4: new FormControl("", Validators.required) ,
+   });
   }
 
-  getColor(color:string): void {
-    if(this.currentColorIdx==0){
-      this.couleursChoisies=["","","",""];
-    }
-    console.log(color);
-    this.couleursChoisies[this.currentColorIdx]=color;
-    this.currentColorIdx++;
-    if(this.currentColorIdx==4){
-      this.currentColorIdx=0;
-    }
+  onSubmit(): void{
+    this.submittedCode = [];
+    this.submittedCode.push(this.formCouleurs.controls.couleur1.value);
+    this.submittedCode.push(this.formCouleurs.controls.couleur2.value);
+    this.submittedCode.push(this.formCouleurs.controls.couleur3.value);
+    this.submittedCode.push(this.formCouleurs.controls.couleur4.value);
+    this.listeTentatives.push(this.submittedCode);
     this.checkGameWon();
   }
 
@@ -44,12 +53,57 @@ export class MastermindComponent implements OnInit {
 
   checkGameWon():void{
     var gameWon=true;
+    this.checkCode();
     for(var i=0; i<this.code.length; i++){
-      if(this.code[i] !== this.couleursChoisies[i]){
+      if(this.code[i] !== this.listeTentatives[this.listeTentatives.length-1][i]){
         gameWon=false;
+        if(this.listeTentatives.length==10){
+          this.lost=true;
+        }
         break;
       }
     }
     this.won=gameWon;
+  }
+
+  checkCode():void{
+    this.submittedResult = [];
+    var codeJson = this.codeJsonConverter(this.code);
+    var codeSubJson = this.codeJsonConverter(this.submittedCode);
+    Object.entries(codeJson).forEach((property: any) => {
+      if(codeSubJson.hasOwnProperty(property[0])){
+        property[1].forEach((c: any) => {
+          if(codeSubJson[property[0]].includes(c)){
+            this.submittedResult.push("ColorOkPlaceOk");
+          }
+          else{
+            this.submittedResult.push("ColorOkPlacePOk");
+          }
+        });
+      }
+      else{
+        property[1].forEach((c: any) => {
+          this.submittedResult.push("None");
+        });
+      }
+    });
+    this.submittedResult.sort();
+    this.listeResultats.push(this.submittedResult);
+    console.log(this.listeResultats);
+  }
+
+  codeJsonConverter(code: string[]):any{
+    var codeJson:any = {};
+    var i =0;
+    code.forEach(c => {
+      if(codeJson.hasOwnProperty(c)){
+        codeJson[c].push(i);
+      }
+      else{
+        codeJson[c]=[i];
+      }
+      i++;
+    });
+    return codeJson;
   }
 }
